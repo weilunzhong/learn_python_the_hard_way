9b129a49ad1726998fc3aec16807ff115901e91e
weilunzhong <weilun@kth.se>
2016-01-08 16:45:15 +0100

    first 11 exs

---
+++ src/ex1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print "hello"
+"""
+123
+123
+"""
+
+# %s for the value and %r for the raw representation
+while True:
+  for i in ["/","-","|","\\","|"]:
+    print "%s\r" % i,
