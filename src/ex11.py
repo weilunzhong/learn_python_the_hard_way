9ca1ac09d15a90a06deb7602443edb4f5376e3b5
weilunzhong <weilun@kth.se>
2016-03-03 15:05:06 +0100

    classmethod test

--- src/ex11.py
+++ src/ex11.py
@@ -1,16 +1,23 @@
-print "How old are you?",
-age = raw_input()
-print "How tall are you?",
-height = raw_input()
-print "How much do you weigh?",
-weight = raw_input()
+# print "How old are you?",
+# age = raw_input()
+# print "How tall are you?",
+# height = raw_input()
+# print "How much do you weigh?",
+# weight = raw_input()
 
-print "So, you're %r old, %r tall and %r heavy." % (
-    age, height, weight)
+# print "So, you're %r old, %r tall and %r heavy." % (
+#     age, height, weight)
 
-age = raw_input("How old are you? ")
-height = raw_input("How tall are you? ")
-weight = raw_input("How much do you weigh? ")
+from sys import argv
 
-print "So, you're %r old, %r tall and %r heavy." % (
-    age, height, weight)
+
+# age = raw_input("How old are you? ")
+# height = raw_input("How tall are you? ")
+# weight = raw_input("How much do you weigh? ")
+
+# print "So, you're %r old, %r tall and %r heavy." % (
+#     age, height, weight)
+
+script, first, second = argv
+
+print script, first, second
